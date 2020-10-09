3e197d369bc49baa2f5818173fcf76face0739e6
Dyang0 <70818105+Dyang0@users.noreply.github.com>
2020-10-09 10:58:50 -0400

    Camera Moves With Player (Scene2)

---
+++ Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //  void Start()
+    //{
+
+    //  }
+
+    public Vector2 speed = new Vector2(50, 50);
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector3(speed.x * inputX, speed.y * inputY);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+
+    }
+}
