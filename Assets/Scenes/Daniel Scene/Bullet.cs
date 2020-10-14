c80dcf27147e894fd5532278ff55401a618a5e5c
Dyang0 <70818105+Dyang0@users.noreply.github.com>
2020-10-14 11:00:59 -0400

    playerController
    Added spinning motion of player and projectiles

---
+++ Assets/Scenes/Daniel Scene/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed; //shoots the bullet in the direction you're facing
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>(); //gets the info of the enemy
+
+        if(enemy != null) //an enemy takes damage respectively if it the enemy is real
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        //Debug.Log(hitInfo.name);  
+        Destroy(gameObject);
+    }*/
+
+
+}
