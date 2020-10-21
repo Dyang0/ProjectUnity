6c1a5708cb0e0b47cad94418f1736029c86800dd
Dyang0 <70818105+Dyang0@users.noreply.github.com>
2020-10-21 11:07:52 -0400

    Circular Gun Motion
    I need to fix bullet travel (the movement of the player affects the bullet movement)

--- Assets/Scenes/Daniel Scene/MouseFollow.cs
+++ Assets/Scenes/Daniel Scene/MouseFollow.cs
@@ -15,7 +15,7 @@ public class MouseFollow : MonoBehaviour
     {
         var pos = Camera.main.WorldToScreenPoint(transform.position);
         var dir = Input.mousePosition - pos;
-        var angle = Mathf.Atan2(dir.y, dir.x-50) * Mathf.Rad2Deg;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
