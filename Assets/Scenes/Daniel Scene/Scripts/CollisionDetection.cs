79843976c6baa21956ecea890ac8fc521e1fea07
Dyang0 <70818105+Dyang0@users.noreply.github.com>
2020-12-04 11:12:12 -0500

    Make sure that Anthony pushes first to keep my saved project
    I don't want to pull Anthony's broken code from last time

--- Assets/Scenes/Daniel Scene/Scripts/CollisionDetection.cs
+++ Assets/Scenes/Daniel Scene/Scripts/CollisionDetection.cs
@@ -13,7 +13,8 @@ public class CollisionDetection : MonoBehaviour
         }
     }
     // Start is called before the first frame update
-    void Start()
+    void Start() //hi
+
     {
         
     }
