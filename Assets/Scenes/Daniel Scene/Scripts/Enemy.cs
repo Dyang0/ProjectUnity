0dc9cec1130f023ab4212ca8a5df2d07208ac55d
Dyang0 <70818105+Dyang0@users.noreply.github.com>
2020-12-18 10:09:36 -0500

    Updated gun controls

---
+++ Assets/Scenes/Daniel Scene/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+    // Update is called once per frame
+    public void loseHealth(int bD)
+    {
+        health -= bD;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+
+            Destroy(collision.gameObject); //deletes bullet object
+            loseHealth(10);  //Make it so the bulletDamage reducces health
+            //loseHealth("pistol");
+        }
+    }
+
+    void Update()
+    {
+
+
+       Debug.Log(health); //shows enemie's health
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+   
+
+
+}
